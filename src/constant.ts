2352eb11f14f9def3efa8ce3f5eb7bc19bdaf9f3
Hapid Fadli <hapidzfadli@gmail.com>
2024-06-28 09:12:09 +0700

    pertama

---
+++ src/constant.ts
@@ -0,0 +1,8 @@
+export const websites = [
+    { name: 'Kiosfilm21', url: 'http://206.189.86.39/' },
+    { name: 'Downmovie21', url: 'https://downmovie21.cloud/' },
+    { name: 'Pusatfilm21', url: 'https://official.pusatfilm21.cloud' },
+    { name: 'Lk21', url: 'http://167.172.4.94/' },
+    { name: 'Indoseries21', url: 'http://104.248.108.43' },
+  ];
+  
