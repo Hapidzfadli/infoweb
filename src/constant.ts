6714fd73c0b556b36ba51f3ba79d011cbbea0039
Hapid Fadli <46638306+Hapidzfadli@users.noreply.github.com>
2024-07-09 01:28:31 +0700

    Update constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,5 @@
 export const websites = [
-    { name: 'Kiosfilm21', url: 'http://kiosfilm21.com/' },
+    { name: 'Kiosfilm21', url: 'http://new.kiosfilm21.com/' },
     { name: 'Downmovie21', url: 'https://new.downmovie21.cloud/' },
     { name: 'Pusatfilm21', url: 'https://official.pusatfilm21.cloud' },
     { name: 'Lk21', url: 'http://68.183.188.69/' },
